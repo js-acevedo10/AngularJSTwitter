ca5b3f1f33554f7a7b39b71d46a6413a4ac5b400
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2015-10-19 01:27:50 -0500

    1st

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: 'bGPtpsnIx0eQJuVtXtHncfZUA',
+		consumer_secret: 'dAg6JZB84XXlKn21VGqxGSofaJtkIaD8c9pUrqCJJbyWSMZ2bD',
+		access_token: '2334095631-QwupYxwRjThfNx7s4j24Vo28ylS64NFs7LA4Fqh',
+		access_token_secret: '4XVrQTuvs02XO6WTqhwJz8Pq2P9m4B00JI4X0lVZnafV9'
+	}
+
+};
